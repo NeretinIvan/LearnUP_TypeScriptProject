23fdc5afb8477f9ef3ed53eed18f6d08bf636d6d
Ivan Neretin <neretinivan17@yandex.ru>
2022-03-06 23:08:49 +0300

    homework-1 done

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWeppackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.ts'),
+    entry: path.resolve(__dirname, 'src/index2.ts'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
